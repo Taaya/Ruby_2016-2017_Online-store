54e82e9b0b25020568126b0bec01ff42cfc30aec
Taya_ <taya.buzukina@gmail.com>
2017-05-08 22:11:56 +0300

    action 'show' added, the first migration created

--- store/app/controllers/items_controller.rb
+++ store/app/controllers/items_controller.rb
@@ -1,7 +1,19 @@
 class ItemsController < ApplicationController
+  def show
+    @item = Item.find(params[:id])
+  end
   def new
   end
   def create
-    render plain: params[:item].inspect
+    @item = Item.new(params.require(:item).permit(:item_name, :description))
+
+    @item.save
+    redirect_to @item
+
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description)
   end
 end
